8ccb1b876724a36183c1e0b13f0e1e317893ec10
Yehonal <yehonal.azeroth@gmail.com>
2021-05-08 20:43:33 +0200

    add definition files

---
+++ deps/eluna-ts-lib/src/PlayerHooks.d.ts
@@ -0,0 +1,3 @@
+export declare function RegisterPlayerOnLogin(listener: OnLogin): void;
+export declare function RegisterPlayerOnLogout(listener: OnLogin): void;
+export declare function RegisterPlayerOnChat(listener: OnChat): void;
