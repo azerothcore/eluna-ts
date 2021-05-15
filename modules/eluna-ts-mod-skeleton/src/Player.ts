2e8202510b33f03bbe804ca3a6575939cc541c23
Yehonal <yehonal.azeroth@gmail.com>
2021-05-15 23:57:15 +0200

    git subrepo pull (merge) Eluna-TS
    subrepo:
      subdir:   "Eluna-TS"
      merged:   "59ab687"
    upstream:
      origin:   "https://github.com/azerothcore/eluna-ts"
      branch:   "master"
      commit:   "136dd51"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- modules/eluna-ts-mod-skeleton/src/Player.ts
+++ modules/eluna-ts-mod-skeleton/src/Player.ts
@@ -3,6 +3,10 @@ import {
   RegisterPlayerOnLogin,
 } from "@azerothcore/eluna-ts-lib";
 
+/**
+ * Example with arrow function
+ */
+
 const OnPlayerLogin: player_event_on_login = (event, player) => {
   player.SendChatMessageToPlayer(
     ChatMsg.CHAT_MSG_WHISPER,
@@ -14,19 +18,38 @@ const OnPlayerLogin: player_event_on_login = (event, player) => {
 
 RegisterPlayerOnLogin(OnPlayerLogin);
 
-const OnPlayerChat: player_event_on_chat = (
-  event,
-  player,
-  msg,
-): string | boolean => {
-  player.SendChatMessageToPlayer(
-    ChatMsg.CHAT_MSG_WHISPER,
-    Language.LANG_UNIVERSAL,
-    `You said: ${msg}`,
+/**
+ *
+ * Example with class
+ *
+ */
+
+class PlayerChat {
+  previousMessage: string;
+
+  constructor(prevMessage: string) {
+    this.previousMessage = prevMessage;
+  }
+
+
+  OnPlayerChat: player_event_on_chat = (
+    event,
     player,
-  );
+    msg,
+  ): string | boolean => {
+    player.SendChatMessageToPlayer(
+      ChatMsg.CHAT_MSG_WHISPER,
+      Language.LANG_UNIVERSAL,
+      `Before you said ${this.previousMessage}, and now you said: ${msg}!`,
+      player,
+    );
 
-  return true;
-};
+    this.previousMessage = msg;
 
-RegisterPlayerOnChat(OnPlayerChat);
+    return true;
+  };
+}
+
+const playerChat = new PlayerChat("First Message");
+
+RegisterPlayerOnChat(playerChat.OnPlayerChat);
