0d07b043f0d10fad91279c1e69797b79b01f05c7
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2021-05-15 22:50:23 +0000

    ci: version bump to v1.0.13

--- deps/eluna-ts-lib/src/declarations/HookHelpers/BattleGroundHooks.d.ts
+++ deps/eluna-ts-lib/src/declarations/HookHelpers/BattleGroundHooks.d.ts
@@ -1,9 +1,9 @@
-export declare function RegisterBattleGroundOnBGStart(hook: bg_event_on_start,
+export declare function RegisterBattleGroundOnBGStart(hook: bg_event_on_start, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterBattleGroundOnBGCreate(hook: bg_event_on_create,
+export declare function RegisterBattleGroundOnBGCreate(hook: bg_event_on_create, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterBattleGroundOnBGDestroy(hook: bg_event_on_pre_destroy,
+export declare function RegisterBattleGroundOnBGDestroy(hook: bg_event_on_pre_destroy, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
--- deps/eluna-ts-lib/src/declarations/HookHelpers/GameObjectHooks.d.ts
+++ deps/eluna-ts-lib/src/declarations/HookHelpers/GameObjectHooks.d.ts
@@ -1,39 +1,39 @@
-export declare function RegisterGameObjectOnDummyEffect(entry: number, hook: gameobject_event_on_dummy_effect,
+export declare function RegisterGameObjectOnDummyEffect(entry: number, hook: gameobject_event_on_dummy_effect, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectUpdateAI(entry: number, hook: gameobject_event_on_aiupdate,
+export declare function RegisterGameObjectUpdateAI(entry: number, hook: gameobject_event_on_aiupdate, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnQuestAccept(entry: number, hook: gameobject_event_on_quest_accept,
+export declare function RegisterGameObjectOnQuestAccept(entry: number, hook: gameobject_event_on_quest_accept, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnQuestReward(entry: number, hook: gameobject_event_on_quest_reward,
+export declare function RegisterGameObjectOnQuestReward(entry: number, hook: gameobject_event_on_quest_reward, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectGetDialogStatus(entry: number, hook: gameobject_event_on_dialog_status,
+export declare function RegisterGameObjectGetDialogStatus(entry: number, hook: gameobject_event_on_dialog_status, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnDestroyed(entry: number, hook: gameobject_event_on_destroyed,
+export declare function RegisterGameObjectOnDestroyed(entry: number, hook: gameobject_event_on_destroyed, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnDamaged(entry: number, hook: gameobject_event_on_damaged,
+export declare function RegisterGameObjectOnDamaged(entry: number, hook: gameobject_event_on_damaged, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnLootStateChanged(entry: number, hook: gameobject_event_on_loot_state_change,
+export declare function RegisterGameObjectOnLootStateChanged(entry: number, hook: gameobject_event_on_loot_state_change, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnGameObjectStateChanged(entry: number, hook: gameobject_event_on_go_state_changed,
+export declare function RegisterGameObjectOnGameObjectStateChanged(entry: number, hook: gameobject_event_on_go_state_changed, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnSpawn(entry: number, hook: gameobject_event_on_spawn,
+export declare function RegisterGameObjectOnSpawn(entry: number, hook: gameobject_event_on_spawn, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnAddToWorld(entry: number, hook: gameobject_event_on_add,
+export declare function RegisterGameObjectOnAddToWorld(entry: number, hook: gameobject_event_on_add, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnRemoveFromWorld(entry: number, hook: gameobject_event_on_remove,
+export declare function RegisterGameObjectOnRemoveFromWorld(entry: number, hook: gameobject_event_on_remove, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
-export declare function RegisterGameObjectOnGameObjectUse(entry: number, hook: gameobject_event_on_use,
+export declare function RegisterGameObjectOnGameObjectUse(entry: number, hook: gameobject_event_on_use, 
 /** the number of times the function will be called, 0 means "always call this function" */
 shots?: number): void;
